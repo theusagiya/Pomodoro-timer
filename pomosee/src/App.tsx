dfc658536884183034a913f0021cab21fb94b2cd
Lucas Mendes <lucamendes.developerjs@gmail.com>
2022-09-02 19:26:40 +0000

    timer add

--- pomosee/src/App.tsx
+++ pomosee/src/App.tsx
@@ -1,22 +1,26 @@
 import { useState } from 'react'
 import './App.css'
-import { activateLasers } from './timer'
+import { activateLasers, stop } from './timer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        
-      </div>
-      <h1>PomoSee</h1>
-      <div className="card">
-        <button onClick={activateLasers}>
-          Ativar lasers
-        </button>
-       
-      </div>
+      <header>
+        <h1>PomoSee</h1>
+        <div>
+          <a>Pomo </a>
+          <a>Break</a>
+        </div> 
+      </header>
+      
+      <div className="card"><div id = "timer">25:00</div></div>
+
+      <footer>
+        <button  id = "pomo" onClick={activateLasers}>START</button>
+        <button  id = "short" className = "show" onClick={activateLasers}>stop</button>
+      </footer>
       
     </div>
   )
--- pomosee/src/timer.tsx
+++ pomosee/src/timer.tsx
@@ -1,8 +1,50 @@
-import React from "react";
 
-export function activateLasers() {
+// Function that implermant start buttom
+function startTimer(duration: number, display: Element  ) {
+  var timer = duration, minutes, seconds;
+  console.log(display);
 
-    return alert('hello');
+  var interval = setInterval(() => {
+    
+  
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+        timer = duration
+    } 
+
+    
+
+  }, 1000)
+
+
+  
+
 
 }
 
+
+
+function timer() {
+  var duration = 60 * 1; 
+  var display = document.querySelector("#timer");
+  return startTimer(duration, display);
+
+}
+
+
+
+
+// Buttom export
+export function activateLasers() {
+
+  return timer();
+}
